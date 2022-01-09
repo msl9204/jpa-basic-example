93b834d65c9df0ee6ea246d40f20cab677cc89f8
minsoo.lee <letomok@gmail.com>
2022-01-09 18:56:07 +0900

    기본 설계 추가

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Order;
+
+import javax.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatue statue;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatue getStatue() {
+        return statue;
+    }
+
+    public void setStatue(OrderStatue statue) {
+        this.statue = statue;
+    }
+}
